2896c5abf29985fde4e667c5de071359626b874e
dmillatiner <dmillatiner@gmail.com>
2019-12-15 18:20:07 +0200

    Played around with the printDeck() function

--- Game.cpp
+++ Game.cpp
@@ -3,5 +3,12 @@
 
 Game::Game(){
    Deck d;
-
+   d.printDeck();
+   std::cout << "\n\n\n";
+   d.setCard(1,1,1);
+   d.setCard(2,1,1);
+   d.setCard(3,1,1);
+   d.setCard(4,1,1);
+   d.setCard(2,2,1);
+   d.printDeck();
 }
