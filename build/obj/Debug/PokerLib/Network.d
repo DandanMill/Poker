d0c253e63a614219259befa48c15190dd1c81e85
dmillatiner <dmillatiner@gmail.com>
2020-01-14 17:45:23 +0200

    gitignore

--- build/obj/Debug/PokerLib/Network.d
+++
@@ -1,8 +0,0 @@
-obj/Debug/PokerLib/Network.o: ../PokerLib/Network.cpp \
- ../includes/Network.h ../includes/Player.h ../includes/Deck.h
-
-../includes/Network.h:
-
-../includes/Player.h:
-
-../includes/Deck.h:
